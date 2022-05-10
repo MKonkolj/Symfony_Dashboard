207c822fac36e8e3c780c2df9d419f6ab035d2d4
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-05-10 10:21:30 +0200

    added user security entity

--- src/Controller/DeveloperController.php
+++ src/Controller/DeveloperController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Developer;
+use App\Repository\DeveloperRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,17 +12,29 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/developer', name: 'developer-')]
 class DeveloperController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    private $em;
+    public function __construct (EntityManagerInterface $em)
     {
+        $this->em = $em;
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(DeveloperRepository $devRepo): Response
+    {
+        $devRepo = $this->em->getRepository(Developer::class);
+
+        $developers = $devRepo->findAll();
+
         return $this->render('developer/index.html.twig', [
-            'controller_name' => 'DeveloperController',
+            'developers' => $developers,
         ]);
     }
 
     #[Route('/{id}', name: 'show')]
-    public function show(): Response
+    public function show(int $id): Response
     {
+        
+
         return $this->render('developer/index.html.twig', [
             'controller_name' => 'DeveloperController',
         ]);
