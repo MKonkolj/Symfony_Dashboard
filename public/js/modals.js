7a761f4f980c3acf10436739ca1d6ce3cba4cf01
Mladen Konkolj <mladenkonkolj@gmail.com>
2022-05-16 14:15:29 +0200

    dodat blako slider za edit user

--- public/js/modals.js
+++ public/js/modals.js
@@ -1,8 +1,12 @@
 let blackAlpha = document.getElementById("blackAlpha")
 let addUserModal = document.getElementById("addUserModal")
 let addClientModal = document.getElementById("addClientModal")
-let openAddUserModalBtn = document.getElementById("openAddUserModalBtn");
-let openAddClientModalBtn = document.getElementById("openAddClientModalBtn");
+let addTaskModal = document.getElementById("addTaskModal")
+let editUserModal = document.getElementById("editUserModal")
+let openTaskModal = document.getElementById("openTaskModal")
+let openAddUserModalBtn = document.getElementById("openAddUserModalBtn")
+let openAddClientModalBtn = document.getElementById("openAddClientModalBtn")
+let openEditUserModalBtn = document.getElementById("openEditUserModalBtn")
 
 blackAlpha.addEventListener("click", () => {
     if (addUserModal) {
@@ -13,6 +17,12 @@ blackAlpha.addEventListener("click", () => {
     }
     if (blackAlpha) {
         blackAlpha.classList.remove("show")
+    }
+    if (addTaskModal) {
+        addTaskModal.classList.remove("show")
+    }
+    if (editUserModal) {
+        editUserModal.classList.remove("show")
     }
 });
 
@@ -27,5 +37,20 @@ if(openAddClientModalBtn) {
     openAddClientModalBtn.addEventListener("click", () => {
         blackAlpha.classList.add("show")
         addClientModal.classList.add("show")
+    })
+}
+
+if(openTaskModal) {
+    openTaskModal.addEventListener("click", () => {
+        blackAlpha.classList.add("show")
+        addTaskModal.classList.add("show")
+    })
+}
+console.log(editUserModal)
+
+if(openEditUserModalBtn) {
+    openEditUserModalBtn.addEventListener("click", () => {
+        blackAlpha.classList.add("show")
+        editUserModal.classList.add("show")
     })
 }
